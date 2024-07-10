dac3e752bc0459df114df7ab2f3b7899fc3dc71d
goto40 <pierre.bayerl@googlemail.com>
2024-07-10 22:00:27 +0200

    service worker

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,4 +1,5 @@
 import { Scene, GameObjects } from 'phaser';
+import { version } from '../../package.json';
 
 export class MainMenu extends Scene
 {
@@ -15,7 +16,7 @@ export class MainMenu extends Scene
     {
         this.background = this.add.image(512, 384, 'background');
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
+        this.title = this.add.text(512, 460, `Main Menu (${version})`, {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
